cc61a72fb30a24dffd1b19ebb28a2e8017468357
Fredy Mosquera <fredy.mosquera@prodigious.com>
2020-07-02 21:54:53 -0500

    adding binding

--- build-a-bot-vuejs/src/App.vue
+++ build-a-bot-vuejs/src/App.vue
@@ -11,19 +11,20 @@
       </nav>
     </header>
     <main>
-      <HomePage/>
+      <RobotBuilder/>
     </main>
 
   </div>
 </template>
 
 <script>
-import HomePage from './components/HomePage.vue';
+
+import RobotBuilder from './build/RobotBuilder.vue';
 
 export default {
   name: 'App',
   components: {
-    HomePage,
+    RobotBuilder,
   },
 };
 </script>
