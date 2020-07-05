a5be39aca1dc74d47323cbf77c1d6825c3b072a2
Fredy Mosquera <fredy.mosquera@prodigious.com>
2020-07-05 11:57:56 -0500

    adding styles conditionally

--- build-a-bot-vuejs/src/build/RobotBuilder.vue
+++ build-a-bot-vuejs/src/build/RobotBuilder.vue
@@ -2,7 +2,7 @@
     <div class="content">
       <button class="add-to-cart" @click="addToCart()">Add to Cart</button>
     <div class="top-row">
-      <div class="top part">
+      <div  :class="[saleBorderClass, 'top', 'part']">
         <div class="robot-name">{{selectedRobot.head.title}}
            <span v-if="selectedRobot.head.onSale" class="sale">Sale!</span>
         </div>
@@ -82,6 +82,14 @@ export default {
     };
   },
   computed: {
+    saleBorderClass() {
+      return this.selectedRobot.head.onSale ? 'sale-border' : '';
+    },
+    headBoderStyle() {
+      return {
+        border: this.selectedRobot.head.onSale ? '3px solid red' : '3px solid #aaa',
+      };
+    },
     selectedRobot() {
       return {
         head: availableParts.heads[this.selectedHeadIndex],
@@ -257,5 +265,8 @@ td, th {
 }
 .cost {
   text-align: right;
+}
+.sale-border {
+  border : 3px solid red;
 }
 </style>
